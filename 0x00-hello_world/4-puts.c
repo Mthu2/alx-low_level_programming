10436d4bed59e8df386b304f7f533bbf3d5f0ab6
Mthu2 <Mthuthuzelimthi@gmail.com>
2023-12-14 00:10:33 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
 /**
- * main - "Programming is like building a multilingual puzzle"
- * Return: 0 
+ * main - Programming with the put puts function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
