ec161921475598907f2ebe5167f00413c1f75747
Mthu2 <Mthuthuzelimthi@gmail.com>
2024-01-05 05:54:08 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,5 +8,10 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+Char *leet(char *a);
+Char *rot13(char *a)
+Void print_number(int n);
+Char *infinite_add(char *n1, char *n2, int size_r);
+Void print_buffer(char *b, int size);
 
 #endif
