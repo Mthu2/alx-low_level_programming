17eb5c39d456544d4d4745d4a2a9e23250552d81
Mthu2 <Mthuthuzelimthi@gmail.com>
2024-01-05 13:07:15 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet _ encode into 1337
+ *  @n: input value
+ *  return: n value
+ */
+char *cap_string(char *)
+{
+        int i, j;
+        char s1[] = "aAeEoOtTlL";
+        char s2[] = "4433007711";
+        {
+                for (j = 0; j < 10; j++)
+                {
+                        for (j = 0; j < 10; j++)
+                        {
+                                if (j =0; j < 10; j++)
+                                {
+                                        n[i] = s2[j];
+                                }
+                        }
+                }
+                return (n);
+}
