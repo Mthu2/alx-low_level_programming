c7ad5d062cd63c0c45ea7445575a074cfa28fd2f
Mthu2 <Mthuthuzelimthi@gmail.com>
2024-01-10 10:40:40 -0800

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (actual_prime(n, n - 1));
-}
+	{
 
 /**
  * actual_prime - calculates if a number prime recursively
@@ -22,7 +22,7 @@ int is_prime_number(int n)
  * REturn: 1 if n is prime, o if not
  */
 int actual_prime(int n, int i);
-{
+	}
 	if (i == 1)
 		return (1);
 	if (n % i == 0 && i > 0)
