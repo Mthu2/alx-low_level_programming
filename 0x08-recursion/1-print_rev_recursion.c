e792df0e300a9f65d36d89809ec90fb1d94a089a
Mthu2 <Mthuthuzelimthi@gmail.com>
2024-01-10 01:31:29 -0800

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function name
+ * @s: paremeter of the function
+ * Return: a string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+}
