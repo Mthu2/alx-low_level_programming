40d9d9ba2397ce8dc4433f4499db737109cf5fb2
Mthu2 <Mthuthuzelimthi@gmail.com>
2024-01-18 22:57:44 -0800

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checke - allocate memory using malloc
+ *
+ * @b: input int
+ * return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+
+	}
+	return (ptr);
+}
