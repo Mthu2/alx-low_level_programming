06b3af7f48b2b61fa28a8cee89d4c51f9cf9c81f
Mthu2 <Mthuthuzelimthi@gmail.com>
2024-01-24 11:13:23 -0800

    ./0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for lowercase
+ * @c: the character to be checked
+ * Return: 1 for uppercase or  0 for others
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
