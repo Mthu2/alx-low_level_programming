f98bf02345b10b645e4a3f1db1b5c99901f356af
Mthu2 <Mthuthuzelimthi@gmail.com>
2024-03-21 05:03:45 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,15 @@ struct dog
 	char *owner;
 };
 
-void int_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-char *_strcpy(char *dest, char src);
-int _strlen(char *s);
+typedef struct dog dog_t;
+
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
 
 #endif
